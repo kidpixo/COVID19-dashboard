f60a5f8a2ea0ef9f05b8742c1f98219fbe67d067
kidpixo <kidpixo@gmail.com>
2020-04-12 09:05:59 +0300

    update 2020-04-12

--- run.sh
+++ run.sh
@@ -7,7 +7,7 @@ CODE=/code
 echo "DIR : $DIR"
 echo "CODE : $CODE"
 # run container 
-docker run  --rm -it -v $DIR:/code covid19-dashboard:alpha jupyter nbconvert --to notebook --inplace --execute $CODE/Logistic\ regression\ COVID-19\ -\ Italy\ dataset.ipynb
+docker run  --rm -it -v $DIR:/code covid19-dashboard:latest jupyter nbconvert --to notebook --inplace --execute $CODE/Logistic\ regression\ COVID-19\ -\ Italy\ dataset.ipynb
 
 git -C $DIR add --all
 git -C $DIR commit --verbose -a -m "update $(date +%F)"
