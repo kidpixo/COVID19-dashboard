8138eb3378aff2be73b29788f65a09acf7ee71a2
kidpixo <kidpixo@gmail.com>
2020-04-12 09:15:20 +0300

    update 2020-04-12

--- run.sh
+++ run.sh
@@ -1,6 +1,9 @@
 ## local dir
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
+## get new version
+git -C $DIR pull
+
 ## this runs inside container
 CODE=/code
 
@@ -8,7 +11,9 @@ echo "DIR : $DIR"
 echo "CODE : $CODE"
 # run container 
 docker run  --rm -it -v $DIR:/code covid19-dashboard:latest jupyter nbconvert --to notebook --inplace --execute $CODE/Logistic\ regression\ COVID-19\ -\ Italy\ dataset.ipynb
+echo "executed $(date +%F)" >> $DIR/execution.log
 
+# pushing upstream
 git -C $DIR add --all
 git -C $DIR commit --verbose -a -m "update $(date +%F)"
 git -C $DIR push
